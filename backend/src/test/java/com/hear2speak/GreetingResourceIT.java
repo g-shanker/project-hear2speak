78eaf4113282bd2ae687e6d4d8f7154ae423e518
crocs-in-socks <gouriswag1@gmail.com>
2025-11-01 14:36:58 +0530

    init backend

---
+++ backend/src/test/java/com/hear2speak/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.hear2speak;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
