75f487c27bf557302063799575a219fa4886915c
crocs-in-socks <gouriswag1@gmail.com>
2025-11-09 17:04:50 +0530

    persisted acknowledgement state in database

--- frontend/src/app/components/appointment-summary-panel/appointment-summary-panel.ts
+++ frontend/src/app/components/appointment-summary-panel/appointment-summary-panel.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AppointmentResponse } from '../../interfaces/appointment-response';
+import { AppointmentService } from '../../services/appointment-service';
+import { ClinicianAppointmentRequest } from '../../interfaces/clinician-appointment-request';
 
 @Component({
   selector: 'app-appointment-summary-panel',
@@ -15,8 +17,41 @@ export class AppointmentSummaryPanel {
 
   selectedAppointment: AppointmentResponse | null = null;
 
+  constructor(private appointmentService: AppointmentService) {}
+
+  updateAppointment(appointment: AppointmentResponse): void {
+    if (appointment) {
+
+      const payload: ClinicianAppointmentRequest = {
+        startDateTime: appointment.startDateTime,
+        patientFullName: appointment.patientFullName,
+        patientEmail: appointment.patientEmail,
+        patientPhoneNumber: appointment.patientPhoneNumber,
+        patientReason: appointment.patientReason,
+        durationInSeconds: appointment.durationInSeconds,
+        appointmentStatus: appointment.appointmentStatus,
+        location: appointment.location,
+        previousAppointmentId: appointment.previousAppointmentId,
+        clinicianNotes: appointment.clinicianNotes,
+        isAcknowledged: appointment.isAcknowledged,
+      }
+
+      this.appointmentService.updateAppointment(appointment.id, payload)
+          .subscribe({
+          next: (response) => {
+            console.log('Appointment updated successfully:', response);
+          },
+          error: (error) => {
+            console.error('Error updating appointment:', payload, error);
+          }
+        });
+    }
+  }
+
   selectAppointment(appointment: AppointmentResponse): void {
+    appointment.isAcknowledged = true;
     this.selectedAppointment = appointment;
     this.appointmentSelected.emit(appointment);
+    this.updateAppointment(appointment);
   }
 }
--- frontend/src/app/services/appointment-service.ts
+++ frontend/src/app/services/appointment-service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PatientAppointmentRequest } from '../interfaces/patient-appointment-request';
 import { AppointmentResponse } from '../interfaces/appointment-response';
+import { ClinicianAppointmentRequest } from '../interfaces/clinician-appointment-request';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,10 @@ export class AppointmentService {
 
   createAppointment(appointment: PatientAppointmentRequest): Observable<PatientAppointmentRequest> {
     return this.http.post<PatientAppointmentRequest>(this.apiUrl, appointment);
+  }
+
+  updateAppointment(appointmentId: number, appointment: ClinicianAppointmentRequest): Observable<ClinicianAppointmentRequest> {
+    return this.http.put<ClinicianAppointmentRequest>(`${this.apiUrl}/${appointmentId}`, appointment);
   }
 
   getAllAppointments(): Observable<AppointmentResponse[]> {
