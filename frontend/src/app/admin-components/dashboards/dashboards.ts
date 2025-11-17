a936705937bf2d2573b1f8f411fc8aada065fd43
crocs-in-socks <gouriswag1@gmail.com>
2025-11-17 23:24:19 +0530

    generated calendar and dashboard components

---
+++ frontend/src/app/admin-components/dashboards/dashboards.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboards',
+  imports: [],
+  templateUrl: './dashboards.html',
+  styleUrl: './dashboards.scss',
+})
+export class Dashboards {
+
+}
--- frontend/src/app/pages/admin/admin.ts
+++ frontend/src/app/pages/admin/admin.ts
@@ -3,6 +3,8 @@ import { Slicer } from '../../admin-components/slicer/slicer';
 import { SlicerItem } from '../../admin-components/slicer/slicer-item.interface';
 import { FindAppointment } from '../../admin-components/find-appointment/find-appointment';
 import { CreateAppointment } from '../../admin-components/create-appointment/create-appointment';
+import { CalendarView } from '../../admin-components/calendar-view/calendar-view';
+import { Dashboards } from '../../admin-components/dashboards/dashboards';
 
 @Component({
     selector: 'app-admin',
@@ -23,6 +25,16 @@ export class Admin {
             title: 'Create Appointment',
             icon: 'add',
             component: CreateAppointment,
+        },
+        {
+            title: 'Calendar View',
+            icon: 'calendar_month',
+            component: CalendarView
+        },
+        {
+            title: 'Dashboards',
+            icon: 'analytics',
+            component: Dashboards
         }
     ]
 }
