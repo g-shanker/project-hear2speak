0133afdc963fce670df6daa8ed835e5becf1d38e
crocs-in-socks <gouriswag1@gmail.com>
2025-12-11 07:39:51 +0530

    updated hero component

---
+++ frontend/src/app/home-components/01-hero/hero.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Carousel } from '../../generic-components/carousel/carousel';
+
+@Component({
+    selector: 'app-hero',
+    standalone: true,
+    imports: [
+        CommonModule,
+        Carousel
+    ],
+    templateUrl: './hero.html',
+    styleUrl: './hero.scss',
+})
+
+export class Hero {
+
+    one = 'assets/images/hero-images/one.jpg'
+    two = 'assets/images/hero-images/two.jpg'
+    three = 'assets/images/hero-images/three.jpg'
+    four = 'assets/images/hero-images/four.jpg'
+    five = 'assets/images/hero-images/five.jpg'
+
+    heroSlides = [
+        this.one,
+        this.two,
+        this.three,
+        this.four,
+        this.five
+    ];
+}
