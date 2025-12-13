51fd061da3e4e20c518a9e68566a7727135f045f
crocs-in-socks <gouriswag1@gmail.com>
2025-12-13 08:58:20 +0530

    updated appointment form component

--- frontend/src/app/domain-components/appointment-form/appointment-form.ts
+++ frontend/src/app/domain-components/appointment-form/appointment-form.ts
@@ -1,10 +1,9 @@
 import { Component, effect, inject, input } from '@angular/core';
-import { AppointmentResponse } from '../../interfaces/appointment-response';
+import { AppointmentResponse } from '../../interfaces/appointment/appointment-response';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { PatientAppointmentRequest } from '../../interfaces/patient-appointment-request';
-import { ClinicianAppointmentRequest } from '../../interfaces/clinician-appointment-request';
-import { AppointmentStatus } from '../../interfaces/appointment-status';
-import { format } from 'date-fns';
+import { AppointmentStatus } from '../../interfaces/appointment/appointment-status';
+import { CreateAppointmentRequest } from '../../interfaces/appointment/create-appointment-request';
+import { UpdateAppointmentRequest } from '../../interfaces/appointment/update-appointment-request';
 
 export type FormMode = 'CLINICIAN' | 'PATIENT';
 
@@ -20,10 +19,12 @@ export class AppointmentForm {
     private formBuilder = inject(FormBuilder);
 
     mode = input<FormMode>('CLINICIAN');
-    initialData = input<AppointmentResponse | null>(null);
-    defaultStartTime = input<string | null>(null);
-    defaultDurationInMinutes = input<number>(30);
 
+    originalData = input<AppointmentResponse | null>(null);
+
+    defaultStartTime = input<string>('');
+    defaultDurationInMinutes = input.required<number>();
+    defaultAppointmentStatus = input.required<AppointmentStatus>();
 
     form = this.formBuilder.group({
 
@@ -31,77 +32,37 @@ export class AppointmentForm {
 
         patientFullName: ['', Validators.required],
         patientEmail: ['', [Validators.required, Validators.email]],
-        patientPhoneNumber: ['', Validators.required],
+        patientPhoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
         patientReason: ['', Validators.required],
-        startDateTime: ['', Validators.required],
+        startDateTime: [this.defaultStartTime(), Validators.required],
 
         // Clinician-specific fields
         
-        appointmentStatus: ['REQUESTED', Validators.required],
-        durationInMinutes: [30, Validators.required],
+        appointmentStatus: [this.defaultAppointmentStatus(), Validators.required],
+        durationInMinutes: [this.defaultDurationInMinutes(), Validators.required],
         clinicianNotes: ['']
     });
 
     constructor() {
         effect(() => {
-            const controls = this.form.controls;
-            if (this.mode() === 'PATIENT') {
-                controls.appointmentStatus.disable();
-                controls.durationInMinutes.disable();
-                controls.clinicianNotes.disable();
-            } else {
-                controls.appointmentStatus.enable();
-                controls.durationInMinutes.enable();
-                controls.clinicianNotes.enable();
-            }
-        });
-
-        effect(() => {
-            const data = this.initialData();
-            const defaultStart = this.defaultStartTime();
-            const defaultDuration = this.defaultDurationInMinutes();
-            if (data) {
+            const originalData = this.originalData();
+            if (originalData) {
                 this.form.patchValue({
-                    ...data,
-                    durationInMinutes: Math.floor(data.durationInSeconds / 60)
-                });
-            } else {
-                this.form.reset({
-                    appointmentStatus: 'SCHEDULED',
-                    startDateTime: defaultStart || '',
-                    durationInMinutes: defaultDuration,
+                    ...originalData,
+                    durationInMinutes: Math.floor(originalData.durationInSeconds / 60)
                 });
             }
         });
     }
 
-    getPatientPayload(): PatientAppointmentRequest | null {
+    getFormContents(): CreateAppointmentRequest | UpdateAppointmentRequest | null {
         if(this.form.invalid) {
             this.form.markAllAsTouched();
             return null;
         }
 
         const raw = this.form.getRawValue();
-        return {
-
-            // Common fields
-
-            patientFullName: raw.patientFullName!,
-            patientEmail: raw.patientEmail!,
-            patientPhoneNumber: raw.patientPhoneNumber!,
-            patientReason: raw.patientReason!,
-            startDateTime: raw.startDateTime!
-        };
-    }
-
-    getClinicianPayload(): ClinicianAppointmentRequest | null {
-        if(this.form.invalid) {
-            this.form.markAllAsTouched();
-            return null;
-        }
-
-        const raw = this.form.getRawValue();
-        const original = this.initialData();
+        const originalData = this.originalData();
 
         return {
 
@@ -115,18 +76,18 @@ export class AppointmentForm {
 
             // Clinician-specific fields
 
-            durationInSeconds: (Number(raw.durationInMinutes) || 30) * 60,
+            durationInSeconds: Number(raw.durationInMinutes) * 60,
             appointmentStatus: raw.appointmentStatus as AppointmentStatus,
-            clinicianNotes: raw.clinicianNotes || '',
-            isAcknowledged: original?.isAcknowledged || false
+            clinicianNotes: raw.clinicianNotes || null,
+            isAcknowledged: originalData?.isAcknowledged || false
         };
     }
 
     reset() {
         this.form.reset({
-            appointmentStatus: 'REQUESTED',
-            durationInMinutes: 30,
-            startDateTime: '',
+            appointmentStatus: this.defaultAppointmentStatus(),
+            durationInMinutes: this.defaultDurationInMinutes(),
+            startDateTime: this.defaultStartTime(),
             patientFullName: '',
             patientEmail: '',
             patientPhoneNumber: '',
